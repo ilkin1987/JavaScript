c1468248cb2b61d1d26014f9b7bd8aa24421b9c8
ilkin1987 <101628203+ilkin1987@users.noreply.github.com>
2022-06-07 22:15:17 +0400

    commit

---
+++ javascript/js/main.js
@@ -0,0 +1,27 @@
+var hour = new Date().getHours();
+if(hour > 6 && hour < 9){
+
+    document.body.style.backgroundColor = "yellow"
+   
+}
+else{
+    document.body.style.backgroundColor = "purple"
+}
+
+
+var hour = new Date().getHours();
+if(hour > 10 && hour < 15){
+    document.body.style.backgroundColor = "red"
+}
+else{
+    document.body.style.backgroundColor = "blue"
+}
+
+var hour = new Date().getHours();
+if(hour > 16 && hour < 22){
+    document.body.style.backgroundColor = "black"
+}
+else{
+    document.body.style.backgroundColor = "orange"
+}
+
